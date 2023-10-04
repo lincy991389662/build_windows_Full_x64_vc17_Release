1b532b57c285ee65e7e753fd4adfe6a5a3e5ac01
侯文峰 <1293434160@qq.com>
2023-10-04 11:32:05 +0800

    feat #0: uv点击展开,提供多种展开,其它隐藏

--- bin/Debug/3.3/scripts/startup/bl_ui/space_image.py
+++ bin/Debug/3.3/scripts/startup/bl_ui/space_image.py
@@ -370,19 +370,19 @@ class IMAGE_MT_uvs_unwrap(Menu):
 
         layout.operator("uv.unwrap")
 
-        layout.separator()
-
-        layout.operator_context = 'INVOKE_DEFAULT'
-        layout.operator("uv.smart_project")
-        layout.operator("uv.lightmap_pack")
-        layout.operator("uv.follow_active_quads")
-
-        layout.separator()
-
-        layout.operator_context = 'EXEC_REGION_WIN'
-        layout.operator("uv.cube_project")
-        layout.operator("uv.cylinder_project")
-        layout.operator("uv.sphere_project")
+        # layout.separator()
+        #
+        # layout.operator_context = 'INVOKE_DEFAULT'
+        # layout.operator("uv.smart_project")
+        # layout.operator("uv.lightmap_pack")
+        # layout.operator("uv.follow_active_quads")
+        #
+        # layout.separator()
+        #
+        # layout.operator_context = 'EXEC_REGION_WIN'
+        # layout.operator("uv.cube_project")
+        # layout.operator("uv.cylinder_project")
+        # layout.operator("uv.sphere_project")
 
 
 class IMAGE_MT_uvs(Menu):
